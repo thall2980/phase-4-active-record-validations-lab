120827673660b249c86fd9001464176d71a08117
Tyler Hall <thall2980@gmail.com>
2022-11-01 12:39:44 -0400

    commit tree a16976e36c796e50eca12e46cb2ad0cbeafaec99

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait?
+
+    def title_is_clickbait?
+        if !title
+            return
+        end
+       unless self.title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+        errors.add(:title, "make it juicier")
+       end
+    end
 end
